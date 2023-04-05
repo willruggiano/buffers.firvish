37efc29b97f515c6cdd9dac821e026d2855463a7
Will Ruggiano <wmruggiano@gmail.com>
2023-04-05 13:07:34 -0600

    fix: terminal

--- lua/buffers-firvish/bufinfo.lua
+++ lua/buffers-firvish/bufinfo.lua
@@ -106,13 +106,21 @@ function TermInfo.new(bufnr, channel)
 end
 
 function TermInfo:running()
-  -- TODO: This doesn't seem correct
-  return vim.fn.jobpid(self.channel) ~= 0
+  local ok, jobpid = pcall(vim.fn.jobpid, self.channel)
+  return ok and jobpid ~= 0
 end
 
 function TermInfo:finished()
   -- TODO: Not sure how to get this one
   return false
+end
+
+function TermInfo:none()
+  if pcall(vim.fn.jobpid, self.channel) then
+    return false
+  else
+    return true
+  end
 end
 
 function BufInfo:terminal()
